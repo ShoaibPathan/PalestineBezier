90b96f72f299ab0c7aa2d27591a3f2bff7dbdea2
Mohammed Abunada <m7606225@gmail.com>
2020-06-17 11:41:07 +0200

    Last update

--- PalestineBezier/ContentView.swift
+++ PalestineBezier/ContentView.swift
@@ -34,20 +34,28 @@ struct PalestineTextView: View {
             .padding(.leading, 50)
             .padding(.trailing, 50)
             .foregroundColor(.white)
-            .background(Capsule()
-                .fill(LinearGradient(gradient: Gradient(colors: [Color(colorBlack),Color(colorBlack), Color(colorRed),Color(colorRed), Color(colorGreen), Color(colorGreen)]), startPoint: .topTrailing, endPoint: .bottomLeading)))
             .font(.largeTitle)
-            .rotation3DEffect(.degrees(scale), axis: (x: 1, y: 0, z: 0))
+            .background(Capsule()
+                .fill(LinearGradient(gradient: Gradient(colors: [
+                    Color(colorBlack),Color(colorBlack),
+                    Color(colorRed),Color(colorRed),
+                    Color(colorGreen), Color(colorGreen)
+                ]), startPoint: .topTrailing, endPoint: .bottomLeading) ) )
+            .rotation3DEffect(.degrees(scale), axis: (x: 1, y: 0, z: 0) )
             .onAppear {
-                withAnimation(Animation.linear(duration: 5.0).repeatForever(autoreverses: true).delay(7.0)){
+                withAnimation(Animation.linear(duration: 5.0)
+                    .repeatForever(autoreverses: true)
+                    .delay(7.0) )
+                {
                     self.scale = 0
                 }
+                
         }//onAppear
     }//body
 }//PalestineTextView
 
 struct PalestineBezierView: View {
-    @State var scale: Double = 180
+    @State var scale: Double = 360
     @State var endAmount: CGFloat = 0
     @State var isFilled = false
     let pathBounds = UIBezierPath.calculateBounds(paths: [palestineBlankLogo])
@@ -57,18 +65,29 @@ struct PalestineBezierView: View {
                 .trim(from: 0, to: endAmount)
                 .stroke(Color(colorRed), lineWidth: 2)
             ShapeView(bezier: palestineBlankLogo, pathBounds: pathBounds)
-                .fill(LinearGradient(gradient: Gradient(colors: [Color(colorBlack),Color(colorBlack), Color(colorRed),Color(colorRed), Color(colorWhite),Color(colorWhite), Color(colorGreen), Color(colorGreen)]), startPoint: .leading, endPoint: .trailing)).opacity(isFilled ? 1 : 0)
+                .fill(LinearGradient(gradient:
+                    Gradient(colors: [
+                        Color(colorBlack),Color(colorBlack),
+                        Color(colorRed),Color(colorRed),
+                        Color(colorWhite),Color(colorWhite),
+                        Color(colorGreen), Color(colorGreen)
+                    ]),startPoint: .leading, endPoint: .trailing))
+                .opacity(isFilled ? 1 : 0)
         }//ZStack
             .frame(width: 200, height: 600 )
-            .rotation3DEffect(.degrees(scale), axis: (x: 0, y: 1, z: 0))
-            .onAppear {
-                withAnimation(.easeInOut(duration: 5)) {
-                    self.endAmount = 1
+            .rotation3DEffect(.degrees(scale), axis: (x: 0, y: 1, z: 0) )
+            .onAppear {withAnimation(.easeInOut(duration: 7) )
+            {
+                self.endAmount = 1
                 }
-                withAnimation(Animation.easeInOut(duration: 3).delay(5)) {
+                withAnimation(Animation.easeInOut(duration: 3).delay(7) )
+                {
                     self.isFilled = true
                 }
-                withAnimation(Animation.linear(duration: 5.0).repeatForever(autoreverses: true).delay(9.0)){
+                withAnimation(Animation.linear(duration: 5.0)
+                    .repeatForever(autoreverses: false)
+                    .delay(9.0) )
+                {
                     self.scale = 0
                 }
         }//onAppear
